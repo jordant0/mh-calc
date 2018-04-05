ef5bf04fbd860a45d7a418b3d832c706984d8b9e
Jordan Tran <kim.tran94@gmail.com>
2018-04-05 17:04:31 -0500

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<script>
+import WeaponInput from '@/components/WeaponInput'
+
+export default {
+  name: 'Main',
+
+  components: {
+    WeaponInput,
+  },
+
+  data() {
+    return {
+      weapon: {
+        raw: 100,
+        affinity: 0,
+        sharpness: 'Green'
+      },
+      skills: [],
+    }
+  }
+}
+</script>
+
+<template>
+  <div class='mh-calc'>
+    <h1>MHW Damage Calculator</h1>
+
+    <weapon-input :weapon='weapon' />
+  </div>
+</template>
---
+++ src/components/WeaponInput.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'WeaponInput',
+
+  props: {
+    weapon: Object,
+  }
+}
+</script>
+
+<template>
+  <form class='weapon-input'>
+    <div>
+      <label>Raw:</label>
+      <input type='text' v-model='weapon.raw' />
+    </div>
+
+    <div>
+      <label>Affinity:</label>
+      <input type='text' v-model='weapon.affinity' />
+    </div>
+
+    <div>
+      <label>Sharpness:</label>
+      <select v-model='weapon.sharpness'>
+        <option>White</option>
+        <option>Blue</option>
+        <option>Green</option>
+        <option>Yellow</option>
+        <option>Red</option>
+      </select>
+    </div>
+  </form>
+</template>
